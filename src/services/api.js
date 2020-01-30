fa54462c6616cfb1ae15ad4c63ea3f466fa59a2e
LauraBeatris <laurabeatriserafim@gmail.com>
2020-01-30 17:17:49 -0300

    products request api fix - work in progress

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.SITE_URL
+        ? `https://${process.env.SITE_URL}`
+        : 'http://localhost:8080',
 });
